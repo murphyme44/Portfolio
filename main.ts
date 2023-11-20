2584f939a0f3f3b38e91aadeac34a34d98e8133b
Michael Murphy <murphyme@udel.edu>
2023-11-20 18:10:00 -0500

    education/tech skills section and minor fixes

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-// Add TypeScript logic if needed
-// For example, you can use TypeScript for interactive features or AJAX requests
-// This file can be compiled to JavaScript using the TypeScript compiler (tsc)
-
-// Example TypeScript code (you can customize this based on your needs)
 document.addEventListener("DOMContentLoaded", function () {
-    // Add your interactive code here
+    // Add interactive code here
 });
